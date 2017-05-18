c391218b1ef97570938b5a4e69eb00b8a6bd1907
sunrain990 <409747494@qq.com>
2017-05-19 02:22:49 +0800

    m shuimin

--- dockerfiles/centos7/ssh/start.sh
+++ dockerfiles/centos7/ssh/start.sh
@@ -4,7 +4,7 @@ __create_user() {
 # Create a user to SSH into as.
 useradd shuimin
 SSH_USERPASS=shuimin
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
+echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin shuimin)
 echo ssh user password: $SSH_USERPASS
 }
 
