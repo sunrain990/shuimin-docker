f3b6fa7c97024ec7045ece627ff0b41ca6599af1
sunrain990 <409747494@qq.com>
2017-05-28 00:57:58 +0800

    add volume +x

--- dockerfiles/centos7/BuildCentOS.sh
+++ dockerfiles/centos7/BuildCentOS.sh
@@ -12,3 +12,10 @@ docker run -d -v /var/lib/mysql --name data-mysql --entrypoint /bin/echo shuimin
 docker run -ti --privileged=true -d --volumes-from data-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.7
 
 #docker run -ti --privileged=true -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.5
+
+# backup && restore
+# ./volume_backup.sh data-mysql /var/lib/mysql
+# docker stop data-mysql && docker rm data-mysql
+# docker run -d -v /var/lib/mysql --name data-mysql --entrypoint /bin/echo shuimin/mysql:5.7
+# ./volume_restore.sh data-mysql
+# docker run -ti --privileged=true -d --volumes-from data-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.7
