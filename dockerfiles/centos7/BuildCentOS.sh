f27a5b0f70b73943cf71cf65c9cf463541684a4c
sunrain990 <409747494@qq.com>
2017-07-18 04:42:33 +0800

    fix static ip

--- dockerfiles/centos7/BuildCentOS.sh
+++ dockerfiles/centos7/BuildCentOS.sh
@@ -2,7 +2,7 @@
 set -e
 docker network create --subnet=172.18.0.0/16 shadownet
 docker build -t shuimin/centos:7.3 .
-docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.4 -p 2222:22 shuimin/centos:7.3
+docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.4 --name=shuimin_centos -p 2222:22  shuimin/centos:7.3
 
 cd mariadb
 
@@ -11,7 +11,7 @@ cd mariadb
 
 docker build -t shuimin/mysql:5.7 .
 docker run -d -v /var/lib/mysql --name data-mysql --entrypoint /bin/echo shuimin/mysql:5.7
-docker run -ti --restart=always --privileged=true -d --volumes-from data-mysql --network=shadownet --ip=172.18.0.5 -p 3307:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.7
+docker run -ti --restart=always --privileged=true -d --volumes-from data-mysql --network=shadownet --ip=172.18.0.5  --name=shuimin_mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.7
 
 #docker run -ti --privileged=true -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.5
 
@@ -22,16 +22,29 @@ docker run -ti --restart=always --privileged=true -d --volumes-from data-mysql -
 # ./volume_restore.sh data-mysql
 # docker run -ti --privileged=true -d --volumes-from data-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root shuimin/mysql:5.7
 
+
+## Backup
+#docker exec CONTAINER /usr/bin/mysqldump -u root --password=root DATABASE > backup.sql
+#
+## Restore
+#cat backup.sql | docker exec -i CONTAINER /usr/bin/mysql -u root --password=root DATABASE
+
 cd ../../project/pond
 
 docker build -t shuimin/pond:0.0.1 .
 
-docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.6 -p 2223:22 shuimin/pond:0.0.1
+docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.6  --name=shuimin_pond -p 2223:22 shuimin/pond:0.0.1
 
 sleep 3 && sync
 
 cd ../wms
 
+echo "heiweigo"
+docker exec shuimin_mysql sh -c 'mysql -h172.18.0.5 -P3306 -uroot -proot --execute="CREATE DATABASE if not exists shuimin_wms;use shuimin_wms;"'
+
+cat wms.sql | docker exec -i shuimin_mysql /usr/bin/mysql -uroot --password=root shuimin_wms
+echo "heiweigo2"
+
 docker build -t shuimin/wms:0.0.1 .
 
-docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.7 -p 2224:22 -p 5584:5584 shuimin/wms:0.0.1
+docker run -ti --restart=always --privileged=true -d --network=shadownet --ip=172.18.0.7 --name=shuimin_wms -p 2224:22 -p 5584:5584 shuimin/wms:0.0.1
--- dockerfiles/project/wms/initdb.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-mysql -h172.18.0.5 -P3306 -uroot -proot --execute="CREATE DATABASE if not exists shuimin_wms;use shuimin_wms;source /opt/workspace/shuimin-wms/wms.sql;"
