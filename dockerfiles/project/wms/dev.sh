3e4473820b47c51f2d57dad481ff7f574249c104
sunrain990 <409747494@qq.com>
2017-07-18 04:01:17 +0800

    fix static ip

--- dockerfiles/project/wms/dev.sh
+++ dockerfiles/project/wms/dev.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+
+mysql -h172.18.0.5 -P3306 -uroot -proot --execute="CREATE DATABASE if not exists shuimin_wms;use shuimin_wms;source /opt/workspace/shuimin-wms/wms.sql;"
+
 mvn clean compile exec:java \
 -Dexec.mainClass=com.shuimin.wms.App \
 -Dpond.web.BaseServer.port=5584 \
